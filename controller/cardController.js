7e15d2bc18670bd731fd4303d2531357bf486fb1
shrutilalwani07 <lalwanishruti72@gmail.com>
2023-08-17 19:15:47 +0530

    Second commit

--- controller/cardController.js
+++ controller/cardController.js
@@ -1,31 +1,31 @@
-// const { features } = require("process");
-// const cardSchema = require("../models/cardSchema")
-// const fs = require("fs");
+const { features } = require("process");
+const cardSchema = require("../models/cardSchema")
+const fs = require("fs");
 
-// //API for add card
-// const addcard = async (req, res) => {
-//     try {
-//       const newcard = new cardSchema (req.body);
-//       if (newcard != null) {
-//         await newcard.save()
-//         res.status(200).json({
-//           success: true,
-//           message: "card added successfully",
-//         });
-//       } else {
-//         res.status(404).json({
-//           success: false,
-//           message: "card not added",
-//         });
-//       }
-//     } catch (error) {
-//       res.status(500).json({
-//         success: false,
-//         error: error.message,
-//       });
-//     }
-//   };
+//API for add card
+const addcard = async (req, res) => {
+    try {
+      const newcard = new cardSchema (req.body);
+      if (newcard != null) {
+        await newcard.save()
+        res.status(200).json({
+          success: true,
+          message: "card added successfully",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "card not added",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  };
 
 
-//   module.exports ={ addcard,}
+  module.exports ={ addcard,}
   
